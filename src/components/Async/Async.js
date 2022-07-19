689cba8bb7638f100ba63ab55fb80b5db175d4ab
Matteo <matteorascioni@gmail.com>
2022-07-19 16:40:17 +0200

    jest tests

---
+++ src/components/Async/Async.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+const Async = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    // fetching data from JSON Placeholder
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => {
+        setPosts(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Async;
